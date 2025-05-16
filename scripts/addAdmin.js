037f514fa62e37c12b3c0315bb144956ed2fa358
Muneeba Marjan <muneebamarjan68@gmail.com>
2025-05-16 12:58:07 +0530

    Initial commit - Digital Identity project structure

--- scripts/addAdmin.js
+++ scripts/addAdmin.js
@@ -4,7 +4,7 @@ const hre = require("hardhat");
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
 
-  const contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const newAdminAddress = "0xdD2FD4581271e230360230F9337D5c0430Bf44C0";
 
   const contractABI = require("../artifacts/contracts/IdentityManagement.sol/IdentityManagement.json").abi;
